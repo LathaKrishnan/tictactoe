0d16bc070661a434dc3f336e133f69cbe7a09b60
Latha <lathanavaneeth@gmail.com>
2019-09-04 14:59:39 +1000

    update

--- game.js
+++ game.js
@@ -8,86 +8,90 @@
 //     }
 // }
 var squares = document.querySelectorAll('.item');
-var myImg = 'tom.gif'
-var Img = 'jerry.gif'
-var player = myImg;
+//var myImg = 'tom.gif'
+//var Img = 'jerry.gif'
+var player = "X";
+//var player = myImg;
+
 var handleClick = function(event){
-    if (event.target.style.backgroundImage === "") {
-        event.target.style.backgroundImage = "url(" + player + ")"
+    if (event.target.textContent === "") {
+        event.target.textContent =  player 
+        "url(" + player + ")"
         Winner()
-        if (player === myImg) {
-            player = Img
+
+        if (player === "X") {
+            player = "O"
         } else {
-            player = myImg
+            player = "X"
         }
     }
 }
 
 var Winner = function (checkWin){
-    if(squares[0].style.backgroundImage ===  myImg &&
-       squares[1].style.backgroundImage ===  myImg &&
-       squares[2].style.backgroundImage ===  myImg
-    ) { alert("Win")}
-    if(squares[0].style.backgroundImage === Img &&
-       squares[1].style.backgroundImage === Img &&
-       squares[2].style.backgroundImage === Img
-    ) { alert("Win")}
-    if(squares[3].style.backgroundImage ===  myImg &&
-    squares[4].style.backgroundImage ===  myImg &&
-    squares[5].style.backgroundImage ===  myImg
-    ) { alert("Win")}
-    if(squares[3].style.backgroundImage === Img &&
-    squares[4].style.backgroundImage === Img &&
-    squares[5].style.backgroundImage === Img
-    ) { alert("Win")}
-    if(squares[6].style.backgroundImage ===  myImg &&
-    squares[7].style.backgroundImage ===  myImg &&
-    squares[8].style.backgroundImage ===  myImg
-    ) { alert("Win")}
-    if(squares[6].style.backgroundImage === Img &&
-    squares[7].style.backgroundImage === Img &&
-    squares[8].style.backgroundImage === Img
-    ) { alert("Win")}
-    if(squares[0].style.backgroundImage ===  myImg &&
-    squares[4].style.backgroundImage ===  myImg &&
-    squares[8].style.backgroundImage ===  myImg
-    ) { alert("Win")}
-    if(squares[0].style.backgroundImage === Img &&
-    squares[4].style.backgroundImage === Img &&
-    squares[8].style.backgroundImage === Img
-    ) { alert("Win")}
-    if(squares[2].style.backgroundImage ===  myImg &&
-    squares[4].style.backgroundImage ===  myImg &&
-    squares[6].style.backgroundImage ===  myImg
-    ) { alert("Win")}
-    if(squares[2].style.backgroundImage === Img &&
-    squares[4].style.backgroundImage === Img &&
-    squares[6].style.backgroundImage === Img
-    ) { alert("Win")}
-    if(squares[0].style.backgroundImage ===  myImg &&
-       squares[3].style.backgroundImage ===  myImg &&
-       squares[6].style.backgroundImage ===  myImg
-    ) { alert("Win")}
-    if(squares[0].style.backgroundImage === Img &&
-       squares[3].style.backgroundImage === Img &&
-       squares[6].style.backgroundImage === Img
-    ) { alert("Win")}
-    if(squares[1].style.backgroundImage ===  myImg &&
-       squares[4].style.backgroundImage ===  myImg &&
-       squares[7].style.backgroundImage ===  myImg
-    ) { alert("Win")}
-    if(squares[1].style.backgroundImage === Img &&
-       squares[4].style.backgroundImage === Img &&
-       squares[7].style.backgroundImage === Img
-    ) { alert("Win")}
-    if(squares[2].style.backgroundImage ===  myImg &&
-       squares[5].style.backgroundImage ===  myImg &&
-       squares[8].style.backgroundImage ===  myImg
-    ) { alert("Win")}
-    if(squares[2].style.backgroundImage === Img &&
-       squares[5].style.backgroundImage === Img &&
-       squares[8].style.backgroundImage === Img
-    ) { alert("Win")}
+    if(squares[0].textContent === "X" &&
+       squares[1].textContent === "X" &&
+       squares[2].textContent === "X"
+    ) {document.querySelector('.winner').textContent = 'Congrats player "X" won the game !!!'}
+    if(squares[0].textContent === "O" &&
+       squares[1].textContent === "O" &&
+       squares[2].textContent === "O"
+    ) { {document.querySelector('.winner').textContent = 'Congrats player "O" won the game !!!'}}
+    if(squares[3].textContent === "X" &&
+    squares[4].textContent === "X" &&
+    squares[5].textContent === "X"
+    ) { {document.querySelector('.winner').textContent = 'Congrats player "X" won the game !!!'}}
+    if(squares[3].textContent === "O" &&
+    squares[4].textContent === "O" &&
+    squares[5].textContent === "O"
+    ) { {document.querySelector('.winner').textContent = 'Congrats player "O" won the game !!!'}}
+    if(squares[6].textContent === "X" &&
+    squares[7].textContent === "X" &&
+    squares[8].textContent === "X"
+    ) { {document.querySelector('.winner').textContent = 'Congrats player "X" won the game !!!'}}
+    if(squares[6].textContent === "O" &&
+    squares[7].textContent === "O" &&
+    squares[8].textContent === "O"
+    ) { {document.querySelector('.winner').textContent = 'Congrats player "O" won the game !!!'}}
+    if(squares[0].textContent === "X" &&
+    squares[4].textContent === "X" &&
+    squares[8].textContent === "X"
+    ) { {document.querySelector('.winner').textContent = 'Congrats player "X" won the game !!!'}}
+    if(squares[0].textContent === "O" &&
+    squares[4].textContent === "O" &&
+    squares[8].textContent === "O"
+    ) { {document.querySelector('.winner').textContent = 'Congrats player "O" won the game !!!'}}
+    if(squares[2].textContent === "X" &&
+    squares[4].textContent === "X" &&
+    squares[6].textContent === "X"
+    ) { {document.querySelector('.winner').textContent = 'Congrats player "X" won the game !!!'}}
+    if(squares[2].textContent === "O" &&
+    squares[4].textContent === "O" &&
+    squares[6].textContent === "O"
+    ) { {document.querySelector('.winner').textContent = 'Congrats player "O" won the game !!!'}}
+    if(squares[0].textContent === "X" &&
+       squares[3].textContent === "X" &&
+       squares[6].textContent === "X"
+    ) { {document.querySelector('.winner').textContent = 'Congrats player "X" won the game !!!'}}
+    if(squares[0].textContent === "O" &&
+       squares[3].textContent === "O" &&
+       squares[6].textContent === "O"
+    ) { {document.querySelector('.winner').textContent = 'Congrats player "O" won the game !!!'}}
+    if(squares[1].textContent === "X" &&
+       squares[4].textContent === "X" &&
+       squares[7].textContent === "X"
+    ) { {document.querySelector('.winner').textContent = 'Congrats player "X" won the game !!!'}}
+    if(squares[1].textContent === "O" &&
+       squares[4].textContent === "O" &&
+       squares[7].textContent === "O"
+    ) { {document.querySelector('.winner').textContent = 'Congrats player "O" won the game !!!'}}
+    if(squares[2].textContent === "X" &&
+       squares[5].textContent === "X" &&
+       squares[8].textContent === "X"
+    ) { {document.querySelector('.winner').textContent = 'Congrats player "X" won the game !!!'}}
+    if(squares[2].textContent === "O" &&
+       squares[5].textContent === "O" &&
+       squares[8].textContent === "O"
+    ) { {document.querySelector('.winner').textContent = 'Congrats player  "O" won the game !!!'}}
 
 }
 
